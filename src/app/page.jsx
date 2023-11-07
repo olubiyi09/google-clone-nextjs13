ab872304f280eb7818b860cf5b28b11d7d2f15c6
olubiyi09 <olubiyi09@gmail.com>
2023-11-08 02:37:01 +0400

    created the jsx for the home header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
+import HomeHeader from '@/components/HomeHeader'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-red-600 text-3xl'>Home</div>
+    <>
+      <HomeHeader />
+    </>
   )
 }
 
