d267ebf355ff90c4b82bdc970a365610ab5c5ffa
olubiyi09 <olubiyi09@gmail.com>
2023-11-12 02:52:10 +0400

    write logic to fetch data from google search api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
 
-const WebSearchPage = () => {
+
+const WebSearchPage = async ({ searchParams }) => {
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    )
+
+    const data = await response.json()
+    const results = data.items
     return (
-        <div>WebSearchPage</div>
+        <>
+            {results && results.map((result, index) =>
+                <h1 key={index}>{result.title}</h1>
+            )}
+        </>
     )
 }
 
