a268ffe56e1871a3aff6660a9481fd1924ba88f7
olubiyi09 <olubiyi09@gmail.com>
2023-11-14 03:08:35 +0400

    Handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 
 const WebSearchPage = async ({ searchParams }) => {
@@ -5,8 +6,28 @@ const WebSearchPage = async ({ searchParams }) => {
         `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
     )
 
+    if (!response.ok) {
+        throw new Error("Something went wrong")
+    }
+
     const data = await response.json()
+
     const results = data.items
+    if (!results) {
+        return (
+
+            <div className="flex flex-col justify-center items-center pt-10">
+                <h1 className="text-3xl mb-4">No results found </h1>
+                <p className="text-lg">Try searching for Something else or go back to the homepage {" "}
+                    <Link href="/" className="text-blue-500">
+                        Home
+                    </Link>
+                </p>
+
+            </div>
+        )
+    }
+
     return (
         <>
             {results && results.map((result, index) =>
