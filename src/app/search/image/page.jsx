b08b2ac8a4e31ec68a2b7e92268ad931d5284fb0
olubiyi09 <olubiyi09@gmail.com>
2023-11-14 22:07:51 +0400

    created the image search results components

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import ImageSearchResults from "@/components/ImageSearchResults"
+import Link from "next/link"
 
-const ImageSearchPage = () => {
+
+const ImageSearchPage = async ({ searchParams }) => {
+    await new Promise((resolve) => setTimeout(resolve, 10000))
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+    )
+
+    if (!response.ok) {
+        throw new Error("Something went wrong")
+    }
+
+    const data = await response.json()
+
+    const results = data.items
+    if (!results) {
+        return (
+
+            <div className="flex flex-col justify-center items-center pt-10">
+                <h1 className="text-3xl mb-4">No results found </h1>
+                <p className="text-lg">Try searching for Something else or go back to the homepage {" "}
+                    <Link href="/" className="text-blue-500">
+                        Home
+                    </Link>
+                </p>
+
+            </div>
+        )
+    }
+
     return (
-        <div>ImageSearchPage</div>
+        <>
+            {results && <ImageSearchResults results={data} />}
+        </>
     )
 }
 
